fa573f086f1a467ca2f9bf6d7544a37eb7c057d7
Apelake25 <alexpeto2000@gmail.com>
2025-04-30 13:10:32 +0200

    Add files via upload

---
+++ Rent_a_car/css/data.js
@@ -0,0 +1,11 @@
+// Ejemplo de estructura de datos en JSON
+const vehiculos = [
+  { modelo: "Toyota Corolla", tipo: "turismo", categoria: "Compacto", precio: 45, estado: "verde" },
+  { modelo: "Yamaha R6", tipo: "moto", categoria: "Deportiva", precio: 35, estado: "naranja" },
+  { modelo: "Renault Kangoo", tipo: "furgoneta", categoria: "Pequeña", precio: 55, estado: "rojo" }
+];
+
+const alquilados = [
+  { modelo: "Seat Ibiza", matricula: "1234XYZ", cliente: "Juan Pérez", desde: "01/04/2025", hasta: "07/04/2025" },
+  { modelo: "Kawasaki Ninja ZX-10R", matricula: "ABC567", cliente: "Laura Torres", desde: "03/04/2025", hasta: "06/04/2025" }
+];
